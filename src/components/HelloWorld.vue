3711e62a09878fc2610fdb13cc0ca4acc856f3a8
Lavish <lavishjain36@gmil.com>
2022-12-28 21:17:14 +0530

    Vue js fetch random Data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,16 @@
 <template>
-  <div id="app">
-    <div class="contain">
-      <h2>Blog Posts</h2>
-      <div class="new">
-        <h3>Add a New Post</h3>
-        <form @submit.prevent="addPost">
-          <input type="text" v-model="newTitle" placeholder="title" required>
-          <input type="text" v-model="newAuthor" placeholder="author name" required>
-        <select name="" id="" v-model="newLabel" required>
-          <option disabled value="">Add a new Label</option>
-          <option >C</option>
-          <option >C++</option>
-          <option>Java</option>
-          <option >Python</option>
-          <option>Perl</option>
+    <div id="app">
+      <img v-bind:src="picture"
+      :alt="`${firstName} ${lastName}`"
+      :class="gender"
+      />
+      <h1>{{ firstName }} {{ lastName }}</h1>
+      <h3>Email:{{ email }}</h3>
+      <h4>Gender:{{ gender }}</h4>
+      <button :class="gender" @click="getUser()">Get a Random User</button>
 
-        </select>
-        <button type="submit">Add a new Blog posts</button>
-        </form>
-      </div>
-      <select  v-model="selected">
-        <option disabled value="">Filter with label</option>
-        <option >C</option>
-          <option >C++</option>
-          <option >Java</option>
-          <option >Python</option>
-          <option >Perl</option>
-      </select>
-      <div v-for="post in filteredByLabel" :key="post.id"  class="post">
-        <span class="label">{{ post.label }}</span>
-        <p>{{ post.title }}</p>
-        <small>{{ post.author }}</small>
-      </div>
+
     </div>
-  </div>
-
 
 </template>
 
@@ -45,68 +21,28 @@ export default {
   props: {
     msg: String
   },
- data(){
-  return{
-   selected:'',
-   posts:[
-    {
-      author:"Yashwant kanetkar",
-      title:'C Language',
-      label:"C"
-    },
-    {
-      author:"Dennis Ritchie",
-      title:'C Language',
-      label:"C"
-    },
-    {
-      author:" Larry Wall ",
-      title:'Perl Language',
-      label:"perl"
-    },
-
-    {
-      author:"James Gosling",
-      title:'Java',
-      label:"Java"
-    },
-    {
-      author:"Orelly ",
-      title:'Java',
-      label:"Java"
-    },
-    {
-      author:"Guido Vann Rossam",
-      title:'Python Language',
-      label:"python"
+  data(){
+    return{
+      firstName:"John",
+      lastName:"Doe",
+      email:"john@gmail.com",
+      gender:"male",
+      picture:"https://randomuser.me/api/portraits/men/60.jpg"
     }
-   ],
-   newTitle:'',
-   newAuthor:'',
-   newLabel:'',
-  }
- },
- methods:{
-  addPost(){
-    let addedPost=new Object({
-      title:this.newTitle,
-      author:this.newAuthor,
-      label:this.newLabel
-    });
-    this.posts.push(addedPost)//added data to posts object
-    this.newTitle='',
-    this.newAuthor='',
-    this.newLabel='';
-  }
- },
+  },
+  methods:{
+    async getUser(){
+      const res=await fetch("https://randomuser.me/api/")
+      const {results}=await res.json()
+      // console.log(results)
 
- computed:{
-  filteredByLabel(){
-    let filter=new RegExp(this.selected,'i')
-    return this.posts.filter(el=>el.label.match(filter))
+      this.firstName=results[0].name.first;
+      this.lastName=results[0].name.last;
+      this.email=results[0].email;
+      this.gender=results[0].gender;
+      this.picture=results[0].picture.large;
+    }
   }
- }
- 
 }
 </script>
 
@@ -129,33 +65,51 @@ a {
 
 
 #app{
+  background-color: rgb(246, 226, 226);
+  width:400px;
+  height: 100vh;
+  margin: auto;
   text-align: center;
-  padding: 70px;
-  font-size:22px;
-  max-width: 360px;
-  margin:0 auto;
-
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
-.contain{
-  widows: 400px;
-  padding: 10px 40px;
+
+h1,h3{
+  margin-bottom: 8px;
+  font-weight: normal;
 }
 
-input,select{
-  font-family: 'Times New Roman', Times, serif;
+img{
+  border-radius: 50%;
+  border:5px solid rgb(255, 0, 191);
+  margin-bottom: 8px;
 }
 
-.new,.post{
-  background:rgb(219, 215, 215);
-  border:1px solid black;
-  border-radius: 10px;
-  padding: 10px 20px;
-  margin:5px 0 10px;
- 
+
+.male{
+  border-color:aquamarine;
+  background-color:rgb(171, 115, 245) ;
 }
-.label{
-  border:1px solid black;
-  background:blue;
+
+.female{
+  border-color:rgb(219, 64, 175);
+  background-color:rgb(245, 245, 115) ;
+}
+
+button{
+  cursor:pointer;
+  display: inline-block;
+  background: white;
+  color:black;
+  font-size: 18px;
+  border:0;
+  border-radius: 20px;
+}
+
+button:hover{
+  background-color: yellow;
 }
 </style>
